b5e8eaaea78830df5f528f0f41dc41c57350f892
ultraeon <robloxrulesmariorules@gmail.com>
2025-04-24 09:02:40 -0500

    start objects

--- tower/the_tower_unsimplified.cpp
+++ tower/the_tower_unsimplified.cpp
@@ -1,6 +1,34 @@
 // unsimplified version
 
-// all variables and objects follow format (name)-(id)
+// all variables and objects follow format (name)g(id)g(id)...
 // variable declaration - item ids
-int coins-
+
+
+// object declaration - group ids corresponding to objects
+// similar objects will share a class with unique names
+// unique objects will share class and object name
+// some groups correspond to both objects and functions, both will be listed separately
+// if objects are numbered they are in left->right order across the level
+SpawnWall spawnWallg1;
+SFX landSFXg2;
+CameraBoundary leftDownCameraBoundaryg3;
+SFX coinSFXg4;
+SFX landSFX2g7;
+Glow torchGlow1g10;
+Glow torchGlow2g11;
+Box box1g22g44;
+Box box4g22g44;
+Box box2g23g46;
+SFX landSFX3g26;
+Vine vine2g27;
+Vine vine4g27;
+Vine vine3g30;
+Vine vine1g31;
+Vine vine5g34;
+Vine vine6g34;
+Box box3g35g44;
+Box box5g44;
+Box box7g44;
+Box box6g46;
+MonsterCage monsterCage
 
