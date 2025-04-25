2419953e755d49063310dcb184adb36837a7878d
ultraeon <robloxrulesmariorules@gmail.com>
2025-04-25 10:56:57 -0500

    a lot of progress on outlining functions

--- tower/the_tower_unsimplified.cpp
+++ tower/the_tower_unsimplified.cpp
@@ -6,7 +6,9 @@ int attempts;
 int mainTime;
 
 // variable declaration - item ids
-
+int timet1; // might be float or double
+int blueCoinsi1;
+int coinsi2;
 
 // object declaration - group ids corresponding to objects
 // similar objects will share a class with unique names
@@ -60,15 +62,39 @@ Text timerTextg303; // at end
 // non event functions
 // functions without a spawn call won't have a group id added
 void setStartPosition() { // built in function
-  player.position = spawnWallg1.position;
+  player.setPosition(spawnWallg1.getPosition());
 }
 
 void setCameraBoundaries() {
-  camera.leftBoundary = leftDownCameraBoundaryg3.x;
-  camera.downBoundary = leftDownCameraBoundaryg3.y;
+  camera.setLeftBoundary(leftDownCameraBoundaryg3.getX());
+  camera.setDownBoundary(leftDownCameraBoundaryg3.getY());
+}
+
+void incrementCoinCounterg4() {
+  coinsi2++;
+}
+
+void startBoxFallg24(int box) {
+  switch(box) {
+    case 1: unk;
+    case 2: unk;
+    case 3: unk;
+    case 4: unk;
+    case 5: unk;
+    case 6: unk;
+    case 7: unk;
+  }
+}
+
+void vineHandlingg28() {
+  unk;
 }
 
 void startColorPulsesg41() {
+  unk;
+}
+
+void startMonsterCageSwingHandlingg49() {
   unk;
 }
 
@@ -80,6 +106,76 @@ void startAcidSkeletonHandlingg54() {
   unk;
 }
 
+void fireballCameraHandlingg69() { // nice id
+  unk;
+}
+
+void resetCameraBeforeFireballsg70() {
+  unk;
+}
+
+void handleFireballg73(int fireball) { // I presume the fireball matters
+  switch(fireball) {
+    unk;
+  }
+}
+
+void handleFireballSequenceg97() { // this might be wrong lmao
+  unk;
+}
+
+void resetCameraAfterFireballsg122() {
+  unk;
+}
+
+void startMovingBridgeHandlingg129() {
+  unk;
+}
+
+void cameraHandlingBeforeMovingSpikesg130() {
+  unk;
+}
+
+void startMovingSawHandlingg136() {
+  unk;
+}
+
+void handleSpinningPlatformsCamerag145() { // broke up into 2 idk if thats best
+  unk;
+}
+
+void handleSpinningPlatformsSFXg145() { // a lot of unks
+  unk;
+}
+
+void startSpinningPlatformsg147() { // naming these things is hard man :(
+  unk;
+}
+
+void startDoorSequenceg192() { // i could switch this to an event function, but I'd rather have it as the events trigger the spawns
+  unk;
+}
+
+void handleBatsg208() { // interacts with g209 ^_^
+  unk;
+}
+
+void startBlueCoin3Lightingg222() { // robtop is really sporadic with his ids, goofy lil fellow
+  unk;
+}
+
+void startBlueCoin1Lightingg224() { // the fog shall devour us all
+  unk;
+}
+
+void stopBlueCoin3Lightingg226() { // why is this neccessary? I have no idea. You do you robtop
+  unk;
+}
+
+void levelEndSequenceg235() { // the end of the level, but not the end of my suffering
+  unk;
+}
+
 void startTowerTextHandlingg296() {
   unk;
 }
@@ -87,10 +183,21 @@ void startTowerTextHandlingg296() {
 void stopTowerTextHandlingg298() {
   towerTextOpacityHandlingg297.toggle(false);
 }
+
+void startEndWalkCamerag305() {
+  unk;
+}
+
+void stopEndWalkCameraWhenBacktrackg307() { // bruh this name is long as hell i gotta make it more concise but don't know how
+  unk;
+}
+
 // events will be defined as functions in the form on(event)
 // for the unsimplified file include all calls, even if redundant
 
-// generally parameters won't be passed in. just treat all variables and objects as having global scope
+// generally parameters will only be passed in for remaps
+// treat all non-parameter variables and objects as having global scope
+
 void onStart() {
   setStartPosition();
   setCameraBoundaries();
@@ -105,4 +212,8 @@ void onStart() {
   unkg67.toggle(false); // function
   unkg302.toggle(false); // function
   unkg306.toggle(false); // function
+}
+
+void onTouchBox(int box) { // parameter is what box is touched - 1-7 based on x position in level
+  startBoxFallg24(box);
 }
