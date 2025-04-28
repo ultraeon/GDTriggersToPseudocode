6881a652e4e15c65311064447c11cc080f1e555e
ultraeon <robloxrulesmariorules@gmail.com>
2025-04-28 09:19:15 -0500

    More object progress

--- tower/the_tower_unsimplified.cpp
+++ tower/the_tower_unsimplified.cpp
@@ -16,32 +16,74 @@ int coinsi2;
 // some groups correspond to both objects and functions, both will be listed separately
 // if objects are numbered they are in left->right order across the level
 
-SpawnWall spawnWallg1;
-Object leftDownCameraBoundaryg3;
-Glow torchGlow1g10;
-Glow torchGlow2g11;
-Box box1g22g44;
+SpawnWall spawnWallg1; // invisible and at spawn
+Auxiliary leftDownBoundaryg3; // at x = 0 y = smth
+Auxiliary alternateTeleportg6; // unused
+Auxiliary towerTextHelperg8; // idk what this is for
+Glow bigTorchGlowg10; // torches scattered throughout level
+Glow smallTorchGlowg11; // ^
+Auxiliary vine2Helperg20; // vine helpers are just used in conjunction with the scale triggers
+WaterfallBackground waterfallBackgroundg21; // waterfalls scattered throughout level
+Box box1g22g44; 
 Box box4g22g44;
 Box box2g23g46;
 Vine vine2g27;
 Vine vine4g27;
+Vine vine8g27;
+Auxiliary vine3Helperg29;
 Vine vine3g30;
 Vine vine1g31;
+Auxiliary vine1Helperg32;
+Auxiliary vine5Helperg33;
 Vine vine5g34;
 Vine vine6g34;
-Box box3g35g44;
+Box box3g35g44; // spawn got added to g35 on this btw
+AcidSplash box2AcidSplashg39; // robtop copy and pasted these a bunch around the level but they're invisible when not animated
+AcidSplash box1AcidSplashg40; // ^
+AcidSplash box4AcidSplashg40;
+AcidSplash box3AcidSplashg43; // this one not everywhere in level(gj robtop)
 Box box5g44g61;
 Box box7g44g65;
 Box box6g46g63;
-MonsterCage monsterCageg47g51;
-Skeleton acidSkeletong53; // theres like 10 of these but share exact behavior
+MonsterCage monsterCageg47g51; // this has a 2 part structure because the chain and cage swing semi-independently
+Auxiliary monsterCageHelperg47g50;
+Chain monsterCageChaing47;
+Auxiliary monsterCageChainHelperg48;
+Plant acidPlant1g53; // theres like 10 of these but share exact behavior
+Plant acidPlant2g55; // again several of these with slightly different behavior from plant1
+Checkpoint checkpoint1g56; // this might be a function idk man
+Checkpoint checkpoint2g56; // ^
+Sparkle checkpointSparkleg57; // 2 total, 1 for each checkpoint
+Particle checkpointParticleg57; // ^
 Vine vine7g59;
+Auxiliary vine7Helperg60;
+AcidSplash box5AcidSplashg62;
+AcidSplash box6AcidSplashg64;
+AcidSplash box7AcidSplashg66;
 CameraGuide fireballCameraGuideg71;
-Particle fireballParticle2g72;
-Particle fireballParticle1g80;
+Particle fireballExplosionParticle2g72;
+Fireball fireball1g74;
+Explosion fireball1Explosiong75;
+CannonBlast fireball1CannonBlastg76g78;
+Glow fireball1CannonGlowg77g78;
+Cannon fireball1Cannong78;
+Auxiliary fireball1CannonHelperg78g79;
+Particle fireballExplosionParticle1g80;
+Glow fireball1ExplosionGlowg81;
+Fireball fireball1g83;
+Explosion fireball2Explosiong84;
+CannonBlast fireball2CannonBlastg85g87;
+Glow fireball2CannonGlowg86g87;
+Cannon fireball2Cannong87;
+Auxiliary fireball2CannonHelperg87g89;
+CannonBlast fireball3CannonBlastg88;
+Glow fireball3CannonGlowg88;
+Cannon fireball3Cannong88;
+Auxiliary fireball3CannonHelperg88;
+Glow fireball2ExplosionGlowg90;
 Particle coinParticleg100; // multiple
 Particle blueCoinParticleg101; // multiple
-Particle fireballParticle3g119;
+Particle fireballExplosionParticle3g119;
 CameraGuide climbCameraGuideg121;
 CameraGuide movingSawCameraGuideg131;
 Saw saw1g132g135;
